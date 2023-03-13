0817da0c9e35d6bb3aaf4880258383d509711e59
Kai Xiang Yong <yongkaixiang@me.com>
2023-03-14 03:00:30 +0800

    add a little swag

--- src/lib/components/LandingPage/Sections/Home.svelte
+++ src/lib/components/LandingPage/Sections/Home.svelte
@@ -1,11 +1,42 @@
 <script>
+  import {deviceHeight} from "../device_state";
+  import {fade} from 'svelte/transition'
+  import {onDestroy, onMount} from "svelte";
+
+  let index = 0;
+  let task = undefined;
+
+  $: imageHeight = $deviceHeight < 320 ? `${$deviceHeight - 32}px` : 'auto'
+
+  onMount(() => {
+      task = setInterval(() => {
+          next();
+      }, 5000)
+  })
+
+  onDestroy(() => {
+      if (task) clearInterval(task);
+  })
+
+  const quotes = [
+      'Swag for life',
+      'Because I can'
+  ]
+
+  const next = () => {
+      index = (index + 1) % quotes.length
+  }
 
 </script>
 
-<div class="flex flex-col items-center justify-center transition-all">
-  <img class="max-h-40" src="https://cdn-icons-png.flaticon.com/512/1012/1012232.png" alt="Under Construction">
-  <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white break-keep md:whitespace-nowrap text-center">
-    Website is under construction.
-  </h1>
-  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-sm md:text-base">Placeholder for name, links, etc</p>
+<div class="flex items-center justify-center transition-all">
+  <img class="max-h-80" style="height: {imageHeight}" src="img.png" alt="Under Construction">
+  <div class="flex flex-col md:pl-6">
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white break-keep md:whitespace-nowrap text-center">
+      Kai Xiang Yong
+    </h1>
+    {#each [quotes[index]] as quote (index)}
+      <p in:fade class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-sm md:text-base">{quote}</p>
+    {/each}
+  </div>
 </div>
