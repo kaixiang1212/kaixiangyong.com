5acddf8d3754bed409abfaae87667491f55fb0b8
Kai Xiang Yong <yongkaixiang@me.com>
2024-01-05 20:58:38 +0800

    change service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,15 @@
 /// <reference types="@sveltejs/kit" />
-import { build, files, version } from '$service-worker';
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+import {build, files, version} from '$service-worker';
 
 // Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
 
 const ASSETS = [
   ...build, // the app itself
-  ...files // everything in `static`
+  ...files  // everything in `static`
 ];
 
 self.addEventListener('install', (event) => {
@@ -40,7 +43,11 @@ self.addEventListener('fetch', (event) => {
 
     // `build`/`files` can always be served from the cache
     if (ASSETS.includes(url.pathname)) {
-      return cache.match(event.request);
+      const response = await cache.match(url.pathname);
+
+      if (response) {
+        return response;
+      }
     }
 
     // for everything else, try the network first, but
@@ -48,13 +55,27 @@ self.addEventListener('fetch', (event) => {
     try {
       const response = await fetch(event.request);
 
+      // if we're offline, fetch can return a value that is not a Response
+      // instead of throwing - and we can't pass this non-Response to respondWith
+      if (!(response instanceof Response)) {
+        throw new Error('invalid response from fetch');
+      }
+
       if (response.status === 200) {
         cache.put(event.request, response.clone());
       }
 
       return response;
-    } catch {
-      return cache.match(event.request);
+    } catch (err) {
+      const response = await cache.match(event.request);
+
+      if (response) {
+        return response;
+      }
+
+      // if there's no cache, then just error out
+      // as there is nothing we can do to respond to this request
+      throw err;
     }
   }
 
