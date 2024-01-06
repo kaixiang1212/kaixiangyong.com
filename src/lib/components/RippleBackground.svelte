9e79bb2322f786a0cc3c00c54484f5549b1109c0
Kai Xiang Yong <yongkaixiang@me.com>
2024-01-06 17:54:27 +0800

    refactor

--- src/lib/components/RippleBackground.svelte
+++ src/lib/components/RippleBackground.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import * as THREE from 'three';
+  import {BufferGeometry} from 'three';
   import {onDestroy, onMount} from 'svelte';
   import {fade} from "svelte/transition";
   import {OrbitControls} from 'three/addons/controls/OrbitControls';
@@ -8,19 +9,26 @@
   let f = 0.002;
   let a = 3;
 
+  const count = 100;
+  const sep = 3;
+
   let scene: THREE.Scene;
   let camera: THREE.PerspectiveCamera;
   let renderer: THREE.WebGLRenderer;
   let controls: OrbitControls;
   let canvas: HTMLCanvasElement;
+  let geometry: BufferGeometry;
 
   onMount(() => {
-    const graph = (x: number, z: number) => {
-      return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
-    };
+    init();
+    animate();
+  });
 
-    const count = 100;
-    const sep = 3;
+  onDestroy(() => {
+    controls?.dispose();
+  });
+
+  function init() {
     const positions = [];
 
     for (let xi = 0; xi < count; xi++) {
@@ -55,7 +63,7 @@
       opacity: 1.0,
     });
 
-    const geometry = new THREE.BufferGeometry();
+    geometry = new THREE.BufferGeometry();
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
 
     const points = new THREE.Points(geometry, material);
@@ -65,46 +73,43 @@
     controls.enableDamping = true;
     controls.dampingFactor = 0.20;
     controls.screenSpacePanning = true;
-    // controls.maxPolarAngle = Math.PI / 2;
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      t += 15;
-
-      const positions = geometry.attributes.position.array;
-      let i = 0;
-
-      for (let xi = 0; xi < count; xi++) {
-        for (let zi = 0; zi < count; zi++) {
-          let x = sep * (xi - count / 2);
-          let z = sep * (zi - count / 2);
-
-          positions[i + 1] = graph(x, z);
-          i += 3;
-        }
-      }
-
-      geometry.attributes.position.needsUpdate = true;
-
-      controls.update();
-      renderer.render(scene, camera);
-    }
-
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    }
 
     window.addEventListener('resize', onWindowResize, false);
+  }
 
-    animate();
-  });
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
-  onDestroy(() => {
-    controls?.dispose();
-  });
+  function graph(x: number, z: number) {
+    return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    t += 15;
+
+    const positions = geometry.attributes.position.array;
+    let i = 0;
+
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+
+        positions[i + 1] = graph(x, z);
+        i += 3;
+      }
+    }
+
+    geometry.attributes.position.needsUpdate = true;
+
+    controls.update();
+    renderer.render(scene, camera);
+  }
 </script>
 
 <style>
